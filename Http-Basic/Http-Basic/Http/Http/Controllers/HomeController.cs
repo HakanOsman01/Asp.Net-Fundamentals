f9de05ebebc7155fb475b898f8a6c1391bb0448c
Hakan Osman <haki2001@abv.bg>
2024-06-18 11:23:12 +0300

    Http-Basic

---
+++ Http-Basic/Http-Basic/Http/Http/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Http.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Http.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MyFirstAction(int id,string name,string email)
+        {
+            Student student = new Student()
+            {
+                Id = id,
+                Name = name,
+                Email = email
+
+
+            };
+            
+            return View(student);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+   
+}
---
+++ Http-Basic/Http-Basic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Http_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
