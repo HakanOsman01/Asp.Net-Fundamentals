9779560910f7178aa42ea9db249524c0b5e6e95f
Hakan Osman <haki2001@abv.bg>
2024-06-18 20:15:26 +0300

    FirstProject

---
+++ ASP.NET Core Introduction/AspNetProject/AspNetProject/Controllers/IntroController.cs
@@ -0,0 +1,34 @@
+﻿using AspNetProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetProject.Controllers
+{
+    public class IntroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetStudent()
+        {
+            ViewBag.Title = "GetStudent";
+            var student = new Student()
+            {
+                Name = "Hakan",
+                Id = 1,
+                Email = "haki2001@abv.bg"
+            };
+            return View("DataStudent",student);
+        }
+        public IActionResult EditStudent(Student model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("DataStudent",model);
+            }
+            ViewBag.Title = "EditStudent";
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
---
+++ ASP.NET Core Introduction/AspNetProject/AspNetProject/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetProject.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
