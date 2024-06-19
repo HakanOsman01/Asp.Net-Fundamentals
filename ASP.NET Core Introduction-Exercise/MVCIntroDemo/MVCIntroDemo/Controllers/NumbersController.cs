5ed2c9f77e89f37242f715c61e90b7c06b0c97a0
Hakan Osman <haki2001@abv.bg>
2024-06-19 15:30:29 +0300

    Numbers Generate

---
+++ ASP.NET Core Introduction-Exercise/MVCIntroDemo/MVCIntroDemo/Controllers/NumbersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCIntroDemo.Controllers
+{
+    public class NumbersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
